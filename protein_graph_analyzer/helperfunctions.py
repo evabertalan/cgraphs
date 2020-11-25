1c5caa507486017ae23e518eef580ccf3a1b5e14
evabertalan <eva0bertalan@gmail.com>
2020-11-25 14:42:41 +0100

    superimpose structures

--- protein_graph_analyzer/helperfunctions.py
+++ protein_graph_analyzer/helperfunctions.py
@@ -15,9 +15,6 @@ amino_d = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
      'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
      'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
 
-def superimpose_structures(self):
-  pass
-
 def get_pdb_files(folder):
     return [file for file in os.listdir(folder) if file.endswith('.pdb')]
 
@@ -44,8 +41,6 @@ def align_sequence(pdb_ref, pdb_move, threshold=0.75):
     ref_sequence = get_sequence(pdb_ref)
     move_sequence = get_sequence(pdb_move)
     alignments = pairwise2.align.globalxx(ref_sequence, move_sequence)
-    print(alignments)
-    print('==============================')
     
     best_score = 0
     best_i = 0
@@ -55,10 +50,15 @@ def align_sequence(pdb_ref, pdb_move, threshold=0.75):
             best_i = i
 
     best_alginment = alignments[best_i]
-    assert len(best_alginment.seqA) == len(best_alginment.seqB), 'Aligned sequences have different lenght'
-    assert best_alginment.score / len(alignments[best_i].seqA) >= threshold, 'Sequences of '+pdb_move+' differ more than the threshold value ('+str(threshold*100)+'%) from the reference structure'
-    assert best_alginment.score / len(alignments[best_i].seqB) >= threshold, 'Sequences of '+pdb_move+' differ more than the threshold value ('+str(threshold*100)+'%) from the reference structure'
-
+    if len(best_alginment.seqA) != len(best_alginment.seqB):
+        print('Aligned sequences have different lenght') #TODO: change all print to log
+        return None, None
+    if best_alginment.score / len(alignments[best_i].seqA) <= threshold:
+        print('Sequences of '+pdb_move+' differ more than the threshold value ('+str(threshold*100)+'%) from the reference structure')
+        return None, None
+    if best_alginment.score / len(alignments[best_i].seqB) <= threshold:
+        print('Sequences of '+pdb_move+' differ more than the threshold value ('+str(threshold*100)+'%) from the reference structure')
+        return None, None
     return best_alginment.seqA, best_alginment.seqB
 
 def superimpose_aligned_atoms(seq_ref, pdb_ref, seq_move, pdb_move, file_name=''):
@@ -69,6 +69,7 @@ def superimpose_aligned_atoms(seq_ref, pdb_ref, seq_move, pdb_move, file_name=''
     ref_struct = parser.get_structure('model', pdb_ref)
     move_struct = parser.get_structure('model', pdb_move)
     
+    #TODO
 #     assert len(ref_struct) == len(move_struct) == 1, 'The reference structure and '+pdb_move+'have more than one protein chain in the provided pdb file.'
     ref_residues = list(ref_struct[0].get_residues())
     move_residues = list(move_struct[0].get_residues())
@@ -76,8 +77,6 @@ def superimpose_aligned_atoms(seq_ref, pdb_ref, seq_move, pdb_move, file_name=''
     for i, r in enumerate(seq_ref):
         for j, m in enumerate(seq_move):
             if ((r == m) and r in amino_d.values() and move_residues[j].get_resname() in amino_d.keys() and ref_residues[i].get_resname() in amino_d.keys() and ref_residues[i].get_id()[1] == move_residues[j].get_id()[1]):
-#                 print(r, m, ref_residues[i], move_residues[j])
-#                 print('======================')
                 ref_atoms.append(ref_residues[i]['CA'])
                 move_atoms.append(move_residues[j]['CA'])
     
@@ -87,7 +86,12 @@ def superimpose_aligned_atoms(seq_ref, pdb_ref, seq_move, pdb_move, file_name=''
     for model in move_struct:
         all_atoms += list(model.get_atoms())
     super_imposer.apply(all_atoms)
-    logging.info('RMS value of superimposed '+pdb_move.split('/')[-1][:-4]+'to the reference structure is '+str(super_imposer.rms))
+#     print(super_imposer.rms)
+    if super_imposer.rms > 5:
+        print('Automatic superimposition of '+file_name+' was not sucessful, please provide a pdb file superimposed to the reference structure')
+        return
+    logging.info('RMS value of superimposed '+file_name+'to the reference structure is '+str(super_imposer.rms))
     io = Bio.PDB.PDBIO()
     io.set_structure(move_struct)
     io.save(file_name+'_superimposed.pdb')
+    
--- protein_graph_analyzer/waterclusters.py
+++ protein_graph_analyzer/waterclusters.py
@@ -2,41 +2,53 @@ import helperfunctions as _hf
 # import numpy as np
 
 class WaterClusters():
-  def __init__(self, pdb_root_folder, target_folder=''):
-    print(pdb_root_folder)
-    self.pdb_root_folder = pdb_root_folder+'/'
-    if target_folder == '':
-      self.target_folder = pdb_root_folder
-    else: self.target_folder = target_folder+'/'
+    def __init__(self, pdb_root_folder, target_folder=''):
+        self.pdb_root_folder = pdb_root_folder+'/'
+        if target_folder == '':
+            self.target_folder = pdb_root_folder+'/'
+        else: self.target_folder = target_folder+'/'
+        
+        self.file_list = _hf.get_pdb_files(self.pdb_root_folder)
+            
+    def align_structures(self, sequance_similarity_threshold=0.75, reference_pdb=''):
+        if reference_pdb == '': reference_pdb = self.file_list[0]
+        print('Reference strucure: ', reference_pdb)
+        
+        for pdb_move in self.file_list[1:]:
+            ref_aligned, move_aligned = _hf.align_sequence(self.pdb_root_folder+reference_pdb,
+                                                       self.pdb_root_folder+pdb_move,
+                                                       threshold=sequance_similarity_threshold)
+            if (ref_aligned is not None) and (move_aligned is not None):
+                _hf.superimpose_aligned_atoms(ref_aligned, self.pdb_root_folder+reference_pdb,
+                                          move_aligned, self.pdb_root_folder+pdb_move,
+                                          file_name= self.target_folder+pdb_move)
 
-    self.file_list = _hf.get_pdb_files(pdb_root_folder)
-#     _hf._superimpose(pdb_root_folder, target_folder)
+            
+    def number_of_waters_per_structure(self):
+        for file in self.file_list:
+            waters = _hf.water_in_pdb(self.pdb_root_folder+file)
+            number_of_waters = len(waters)
+            print(number_of_waters)
 
-  def number_of_waters_per_structure(self):
-    for file in self.file_list:
-      waters = _hf.water_in_pdb(self.pdb_root_folder+file)
-      number_of_waters = len(waters)
-      print(number_of_waters)
+    def fit_parameters(self):
+        pass
 
-  def fit_parameters(self):
-    pass
+    def evaluate_parameters(self):
+        #write to file
+        pass
 
-  def evaluate_parameters(self):
-    #write to file
-    pass
+    def calculate_cluster_centers(self):
+        pass
 
-  def calculate_cluster_centers(self):
-    pass
+    def plot_clusters(self):
+        pass
 
-  def plot_clusters(self):
-    pass
+    def draw_cluster_centers(self):
+        pass
 
-  def draw_cluster_centers(self):
-    pass
-
-  def calculate_water_clusters(self):
-    self.fit_parameters()
-    self.evaluate_parameters()
-    self.calculate_cluster_centers()
-    self.plot_clusters()
-    self.draw_cluster_centers()
+    def calculate_water_clusters(self):
+        self.fit_parameters()
+        self.evaluate_parameters()
+        self.calculate_cluster_centers()
+        self.plot_clusters()
+        self.draw_cluster_centers()
