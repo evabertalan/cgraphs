271667dd3c25a06ef77aed2d3f2692a91cff3e5a
evabertalan <eva0bertalan@gmail.com>
2020-12-29 14:15:25 +0100

    basic graphical interface

--- protein_graph_analyzer/view.py
+++ protein_graph_analyzer/view.py
@@ -1,56 +1,106 @@
 import tkinter as tk
+import os
 from tkinter import filedialog
 import waterclusters as wc
-import matplotlib.pyplot as plt
+import conservedgraph as cg
+
 
 
 class View:
   def __init__(self, master):
     self.master = master
+    self.ipad = 12
+    self.pad = 6
 
   def start(self):
     if hasattr(self, 'mainframe'):
       self._destroy_frame()
 
     self.master.title('Protein graph analyser')
-    self.master.geometry('650x650')
+    self.master.geometry('550x550')
     self._create_frame()
-    tk.Label(self.mainframe, text='Please select a folder, which contains all the PDB files you want to include in the analysis').grid(row=0, column=0)
-    tk.Button(self.mainframe, text='Select Folder', command=self._select_root_folder).grid(row=0, column=1)
 
-    tk.Label(self.mainframe, text='Select refernece file').grid(row=2, column=0)
-    tk.Button(self.mainframe, text='Select refernece file', command=self._select_reference_file).grid(row=2, column=1)
 
-    tk.Label(self.mainframe, text='Water Cluster Analysis').grid(row=4, column=0)
-    tk.Button(self.mainframe, text='Calculate water clustes', command=self._init_water_clusters).grid(row=4, column=1)
+    self.inputFrame = tk.LabelFrame(self.mainframe, text='Input Locations')
+    self.inputFrame.grid(row=0, columnspan=3, sticky='EW', padx=(self.pad,self.pad), pady=(self.pad,self.pad), ipadx=self.ipad, ipady=self.ipad)
+    self.inputFrame.columnconfigure(0, weight=1)
+    self.inputFrame.columnconfigure(1, weight=1)
 
-    tk.Label(self.mainframe, text='Conserved graph analysis').grid(row=6, column=0)
-    tk.Button(self.mainframe, text='Calculate conserved H-bond network', command=self._init_conserved_graph_analysis).grid(row=6, column=1)
+    tk.Button(self.inputFrame, text='Select PDB Folder', command=self._select_root_folder).grid(row=1, column=0, sticky="EW")
+    s1 = self._add_horisontal_scroll(self.inputFrame, row=2, column=1)
+    self._input_folder = tk.Entry(self.inputFrame, state='disabled', xscrollcommand=s1.set)
+    self._input_folder.grid(row=1, column=1, sticky="EW", columnspan=2)
+    s1.configure(command = self._input_folder.xview)
 
-    useWaterCoords = tk.BooleanVar(self.mainframe, False)
-    tk.Checkbutton(self.mainframe, text='Use water cluster coordinates', variable=useWaterCoords).grid(row=7, column=1)
+    tk.Button(self.inputFrame, text='Select refernece file', command=self._select_reference_file).grid(row=4, column=0, sticky="EW")
+    s2 = self._add_horisontal_scroll(self.inputFrame, row=5, column=1)
+    self._input_pdb = tk.Entry(self.inputFrame, state='disabled', xscrollcommand=s2.set)
+    self._input_pdb.grid(row=4, column=1, sticky="EW")
+    s2.configure(command = self._input_pdb.xview)
 
-    tk.Button(self.mainframe, text='Calculate conserved water wire network', command=self._init_conserved_graph_analysis).grid(row=6, column=1)
+
+    self.waterClusterFrame = tk.LabelFrame(self.mainframe, text='Water Cluster Analysis')
+    self.waterClusterFrame.grid(row=4, columnspan=3, sticky='EW', padx=(self.pad,self.pad), pady=(self.pad,self.pad), ipadx=self.ipad, ipady=self.ipad)
+    self.waterClusterFrame.columnconfigure(0, weight=1)
+
+    tk.Button(self.waterClusterFrame, text='Calculate water clustes', command=self._init_water_clusters).grid(row=4, column=0, padx=(self.pad,self.pad), pady=(self.pad,self.pad), sticky="EW")
+
+
+    self.conservedNetworkFrame = tk.LabelFrame(self.mainframe, text='Conserved network analysis')
+    self.conservedNetworkFrame.grid(row=6, columnspan=3, sticky='EW', padx=(self.pad,self.pad), pady=(self.pad,self.pad), ipadx=self.ipad, ipady=self.ipad)
+    self.conservedNetworkFrame.columnconfigure(0, weight=1)
+    self.conservedNetworkFrame.columnconfigure(1, weight=1)
+    tk.Button(self.conservedNetworkFrame, text='Calculate conserved H-bond network', command=lambda:self._init_conserved_graph_analysis('hbond')).grid(row=7, column=0, padx=(self.pad,self.pad), pady=(self.pad,self.pad), sticky="EW")
+
+    self.useWaterCoords = tk.BooleanVar(self.mainframe, False)
+    tk.Checkbutton(self.conservedNetworkFrame, text='Use water cluster coordinates', variable=self.useWaterCoords).grid(row=7, column=1, padx=(self.pad,self.pad), pady=(self.pad,self.pad))
+
+    tk.Button(self.conservedNetworkFrame, text='Calculate conserved water wire network', command=lambda:self._init_conserved_graph_analysis('water_wire')).grid(row=8, column=0, padx=(self.pad,self.pad), pady=(self.pad,self.pad), sticky="EW")
+
 
   def _select_root_folder(self):
     self.pdb_root_folder = filedialog.askdirectory(initialdir = "../")
-    tk.Label(self.mainframe, text='Selected Folder:'+str(self.pdb_root_folder)).grid(row=1, column=0)
+    self._input_folder.configure(state='normal')
+    self._input_folder.insert(0, str(self.pdb_root_folder))
+    self._input_folder.configure(state='disabled')
+
 
   def _select_reference_file(self):
     self.reference_pdb = filedialog.askopenfilename(initialdir = "../")
-    tk.Label(self.mainframe, text='Selected Folder:'+str(self.reference_pdb)).grid(row=3, column=0)
+    self._input_pdb.configure(state='normal')
+    self._input_pdb.insert(0, str(self.reference_pdb))
+    self._input_pdb.configure(state='disabled')
+
+    tk.Label(self.mainframe, text='All the generated files can be found in: '+self.pdb_root_folder+'/workfolder/\n Plots are located in '+self.pdb_root_folder+'/workfolder/plots/', wraplength=50).grid(row=10, column=0, columnspan=3)
 
   def _init_water_clusters(self):
     w = wc.WaterClusters(self.pdb_root_folder, reference_pdb=self.reference_pdb)
-    tk.Label(self.mainframe, text='There are '+str(len(w.water_coordinates))+' water molecules in the '+str(len(w.superimposed_files))+' superimpsed files').grid(row=5, column=0)
-    fig, ax = w.evaluate_parameters()
-    plt.show()
+    w.evaluate_parameters()
+    w.calculate_cluster_centers()
+    self.ref_coordinates = w.reference_coordinates
+    tk.Label(self.waterClusterFrame, text='There are '+str(len(w.water_coordinates))+' water molecules in the '+str(len(w.superimposed_files))+' superimpsed files.\n The algorithm found '+str(w.n_clusters_)+' water clusters.').grid(row=5, column=0)
 
-  def _init_conserved_graph_analysis(self):
-    pass
+  def _init_conserved_graph_analysis(self, graph_type):
+    if self.useWaterCoords: _ref_coord = self.ref_coordinates
+    else: _ref_coord=None
+    c = cg.ConservedGraph(self.pdb_root_folder, reference_pdb=self.reference_pdb, reference_coordinates=_ref_coord)
+    c.calculate_graphs(graph_type=graph_type)
+    c.plot_graphs()
+    c.get_conserved_graph()
+    c.plot_conserved_graph()
+    tk.Label(self.conservedNetworkFrame, text='Calculation completed', fg='green').grid(row=9, column=0)
+
+  def _add_horisontal_scroll(self, target, row=1, column=0):
+    scroll = tk.Scrollbar(target, orient='horizontal')
+    scroll.grid(row=row, column=column, sticky='EW')
+    return scroll
 
 
+  def on_enter(self, event, text):
+        self.l2.configure(text=text)
 
+  def on_leave(self, enter):
+        self.l2.configure(text='')
 
   def _destroy_frame(self):
     self.mainframe.destroy()
