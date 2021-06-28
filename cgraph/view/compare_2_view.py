f81c064728a678a35075b2cd7decdd85a87fcbed
evabertalan <eva0bertalan@gmail.com>
2021-06-28 07:18:44 +0200

    fix ugly colors and highlights in dark mode

--- cgraph/view/compare_2_view.py
+++ cgraph/view/compare_2_view.py
@@ -7,24 +7,24 @@ def compare_view(self):
     file_selector.columnconfigure(0, weight=1)
     file_selector.columnconfigure(1, weight=1)
 
-    tk.Button(file_selector, text='Location of workfolder', command=lambda:self._set_compare_result_folder(compare_results_input), takefocus=False).grid(row=0, column=0, sticky="EW")
+    tk.Button(file_selector, text='Location of workfolder', command=lambda:self._set_compare_result_folder(compare_results_input), takefocus=False, bg='white', fg='black', highlightbackground='white').grid(row=0, column=0, sticky="EW")
     s3 = self._add_horisontal_scroll(file_selector, row=1, column=1)
-    compare_results_input = tk.Entry(file_selector, state='disabled', xscrollcommand=s3.set)
+    compare_results_input = tk.Entry(file_selector, state='disabled', xscrollcommand=s3.set, highlightbackground='white', bg='white', fg='black', disabledbackground=self.gray, disabledforeground='black')
     compare_results_input.grid(row=0, column=1, sticky="EW")
-    s3.configure(command=compare_results_input.xview)
+    s3.configure(command=compare_results_input.xview, bg='white')
 
-    tk.Label(file_selector, text='  H-bond criteria ', anchor="w").grid(row=2, column=0, sticky='W')
-    comp_hcritera_frame = tk.Frame(file_selector)
+    tk.Label(file_selector, text='  H-bond criteria ', anchor="w", bg='white', fg='black').grid(row=2, column=0, sticky='W')
+    comp_hcritera_frame = tk.Frame(file_selector,bg='white')
     comp_hcritera_frame.grid(row=2, column=1, columnspan=4, sticky="EW")
     self.comp_distance = tk.DoubleVar(value=3.5)
     self.comp_cut_angle = tk.DoubleVar(value=60)
     ttk.Spinbox(comp_hcritera_frame, textvariable=self.comp_distance, from_=0, to=5, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 5)).grid(row=0, column=1, sticky='W')
-    tk.Label(comp_hcritera_frame, text='Å distance     ', anchor="w").grid(row=0, column=2, sticky='W')
+    tk.Label(comp_hcritera_frame, text='Å distance     ', anchor="w", bg='white', fg='black').grid(row=0, column=2, sticky='W')
     self.comp_use_angle = tk.BooleanVar()
     self.comp_use_angle.set(False)
-    tk.Checkbutton(comp_hcritera_frame, variable=self.comp_use_angle, anchor="w").grid(row=0, column=3, sticky='E')
+    tk.Checkbutton(comp_hcritera_frame, variable=self.comp_use_angle, anchor="w", bg='white', fg='black').grid(row=0, column=3, sticky='E')
     ttk.Spinbox(comp_hcritera_frame, textvariable=self.comp_cut_angle, from_=0, to=180, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 180)).grid(row=0, column=4, sticky='W')
-    tk.Label(comp_hcritera_frame, text='degrees angle', anchor="w").grid(row=0, column=5, sticky='W')
+    tk.Label(comp_hcritera_frame, text='degrees angle', anchor="w", bg='white', fg='black').grid(row=0, column=5, sticky='W')
 
     comparison_frame = ttk.LabelFrame(self.compframe, text='Compare networks')
     comparison_frame.grid(self._crate_frame_grid(2))
@@ -42,21 +42,21 @@ def compare_view(self):
 
     # ------------------- PDB tab -----------------------
 
-    tk.Button(pdb_compare_tab, text='PDB 1', command=lambda:self._select_pdb1(pdb1_field), takefocus=False).grid(row=2, column=0, sticky="EW")
+    tk.Button(pdb_compare_tab, text='PDB 1', command=lambda:self._select_pdb1(pdb1_field), takefocus=False, bg='white', fg='black', highlightbackground='white').grid(row=2, column=0, sticky="EW")
     s1 = self._add_horisontal_scroll(pdb_compare_tab, row=3, column=1)
-    pdb1_field = tk.Entry(pdb_compare_tab, state='disabled', xscrollcommand=s1.set)
+    pdb1_field = tk.Entry(pdb_compare_tab, state='disabled', xscrollcommand=s1.set, bg='white', fg='black', highlightbackground='white', disabledbackground=self.gray, disabledforeground='black')
     pdb1_field.grid(row=2, column=1, sticky="EW")
-    s1.configure(command=pdb1_field.xview)
+    s1.configure(command=pdb1_field.xview, bg='white')
     self.color1 = '#1b3ede'
     color_field1 = tk.Label(pdb_compare_tab, width=2, bg=self.color1, anchor="w")
     color_field1.grid(row=2, column=2, sticky='W')
     color_field1.bind("<Button-1>", lambda x=self.color1, y=color_field1:self._choose_color1(x, y))
 
-    tk.Button(pdb_compare_tab, text='PDB 2', command=lambda:self._select_pdb2(pdb2_field)).grid(row=4, column=0, sticky="EW")
+    tk.Button(pdb_compare_tab, text='PDB 2', command=lambda:self._select_pdb2(pdb2_field), bg='white', fg='black',highlightbackground='white').grid(row=4, column=0, sticky="EW")
     s2 = self._add_horisontal_scroll(pdb_compare_tab, row=5, column=1)
-    pdb2_field = tk.Entry(pdb_compare_tab, state='disabled', xscrollcommand=s2.set)
+    pdb2_field = tk.Entry(pdb_compare_tab, state='disabled', xscrollcommand=s2.set, bg='white', fg='black', highlightbackground='white', disabledbackground=self.gray, disabledforeground='black')
     pdb2_field.grid(row=4, column=1, sticky="EW")
-    s2.configure(command=pdb2_field.xview)
+    s2.configure(command=pdb2_field.xview, bg='white')
     self.color2 = '#21c25f'
     color_field2 = tk.Label(pdb_compare_tab, width=2, bg=self.color2, anchor="w")
     color_field2.grid(row=4, column=2, sticky='W')
@@ -71,12 +71,12 @@ def compare_view(self):
 
     self.include_waters_comp = tk.BooleanVar()
     self.include_waters_comp.set(True)
-    tk.Checkbutton(hbond_frame, text='Include crystallographic waters', variable=self.include_waters_comp, anchor="w").grid(self._create_big_button_grid(8))
+    tk.Checkbutton(hbond_frame, text='Include crystallographic waters', variable=self.include_waters_comp, anchor="w", bg='white', fg='black').grid(self._create_big_button_grid(8))
 
     self.include_backbone_sidechain_comp = tk.BooleanVar()
-    tk.Checkbutton(hbond_frame, text='Include sidechain-backbone interactions', variable=self.include_backbone_sidechain_comp, anchor="w").grid(self._create_big_button_grid(9))
+    tk.Checkbutton(hbond_frame, text='Include sidechain-backbone interactions', variable=self.include_backbone_sidechain_comp, anchor="w", bg='white', fg='black').grid(self._create_big_button_grid(9))
 
-    tk.Button(hbond_frame, text='Compare H-bond networks', command=lambda:self._init_pdb_comparison(comp_type='hbond', pdb1=self.pdb_1, pdb2=self.pdb_2, color1=self.color1, color2=self.color2), width=self.button_width).grid(self._create_big_button_grid(10), columnspan=2)
+    tk.Button(hbond_frame, text='Compare H-bond networks', command=lambda:self._init_pdb_comparison(comp_type='hbond', pdb1=self.pdb_1, pdb2=self.pdb_2, color1=self.color1, color2=self.color2), width=self.button_width, bg='white', fg='black',highlightbackground='white').grid(self._create_big_button_grid(10), columnspan=2)
 
 
     # # -------------------water_wire_frame -----------------------
@@ -87,30 +87,30 @@ def compare_view(self):
     water_wire_frame.columnconfigure(1, weight=1)
 
     self.max_water_comp = tk.IntVar(value=3)
-    tk.Label(water_wire_frame, text='Maximum number of waters in the bridge', anchor="w").grid(row=11, column=0, sticky='W')
+    tk.Label(water_wire_frame, text='Maximum number of waters in the bridge', anchor="w", bg='white', fg='black').grid(row=11, column=0, sticky='W')
     ttk.Combobox(water_wire_frame, textvariable=self.max_water_comp, values=[1,2,3,4,5], state='readonly').grid(row=11, column=1, sticky="EW")
-    tk.Button(water_wire_frame, text='Compare water wire network', command=lambda:self._init_pdb_comparison(comp_type='water_wire' ,pdb1=self.pdb_1, pdb2=self.pdb_2, color1=self.color1, color2=self.color2), width=self.button_width).grid(self._create_big_button_grid(12), columnspan=2)
+    tk.Button(water_wire_frame, text='Compare water wire network', command=lambda:self._init_pdb_comparison(comp_type='water_wire' ,pdb1=self.pdb_1, pdb2=self.pdb_2, color1=self.color1, color2=self.color2), width=self.button_width, bg='white', fg='black', highlightbackground='white').grid(self._create_big_button_grid(12), columnspan=2)
 
     # # ------------------- TRAJECTORY tab -----------------------
 
-    tk.Button(self.dcd_compare_tab, text='PSF 1', command=lambda:self._select_psf1_compare(psf1_field), takefocus=False).grid(row=2, column=0, sticky="EW")
+    tk.Button(self.dcd_compare_tab, text='PSF 1', command=lambda:self._select_psf1_compare(psf1_field), takefocus=False, bg='white', fg='black', highlightbackground='white').grid(row=2, column=0, sticky="EW")
     s3 = self._add_horisontal_scroll(self.dcd_compare_tab, row=3, column=1)
-    psf1_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s3.set)
+    psf1_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s3.set,  highlightbackground='white', bg='white', fg='black', disabledbackground=self.gray, disabledforeground='black')
     psf1_field.grid(row=2, column=1, sticky="EW")
-    s3.configure(command=psf1_field.xview)
+    s3.configure(command=psf1_field.xview, bg='white')
 
-    tk.Button(self.dcd_compare_tab, text='DCDs 1', command=lambda:self._select_dcd1_compare(dcd1_field)).grid(row=4, column=0, sticky="EW")
+    tk.Button(self.dcd_compare_tab, text='DCDs 1', command=lambda:self._select_dcd1_compare(dcd1_field), bg='white', fg='black', highlightbackground='white').grid(row=4, column=0, sticky="EW")
     s4 = self._add_horisontal_scroll(self.dcd_compare_tab, row=5, column=1)
-    dcd1_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s4.set)
+    dcd1_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s4.set, highlightbackground='white', bg='white', fg='black', disabledbackground=self.gray, disabledforeground='black')
     dcd1_field.grid(row=4, column=1, sticky="EW")
-    s4.configure(command=dcd1_field.xview)
+    s4.configure(command=dcd1_field.xview, bg='white')
 
-    tk.Label(self.dcd_compare_tab, text='Name 1: ', anchor='w').grid(row=6, column=0, sticky='W')
+    tk.Label(self.dcd_compare_tab, text='Name 1: ', anchor='w', bg='white', fg='black').grid(row=6, column=0, sticky='W')
     # self.sim_name1 = tk.StringVar(value='sim1')
-    field1 = tk.Frame(self.dcd_compare_tab)
+    field1 = tk.Frame(self.dcd_compare_tab, bg='white')
     field1.grid(row=6, column=1, sticky="EW", pady=(0,12))
     field1.columnconfigure(0, weight=1)
-    self.compare_dcd1_name = tk.Entry(field1)
+    self.compare_dcd1_name = tk.Entry(field1, bg='white', fg='black', highlightbackground='white')
     self.compare_dcd1_name.insert(0, 'sim1') # remove when test resolved
     self.compare_dcd1_name.grid(row=6, column=0, sticky="EW")
     self.color_dcd1 = '#1b3ede'
@@ -118,27 +118,27 @@ def compare_view(self):
     color_dcd_field1.grid(row=6, column=1, sticky='W')
     color_dcd_field1.bind("<Button-1>", lambda x=self.color_dcd1, y=color_dcd_field1:self._choose_dcd_color1(x, y))
 
-    tk.Button(self.dcd_compare_tab, text='PSF 2', command=lambda:self._select_psf2_compare(psf2_field)).grid(row=7, column=0, sticky="EW")
+    tk.Button(self.dcd_compare_tab, text='PSF 2', command=lambda:self._select_psf2_compare(psf2_field), bg='white', fg='black', highlightbackground='white').grid(row=7, column=0, sticky="EW")
     s5 = self._add_horisontal_scroll(self.dcd_compare_tab, row=8, column=1)
-    psf2_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s5.set)
+    psf2_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s5.set, bg='white', fg='black', highlightbackground='white', disabledbackground=self.gray, disabledforeground='black')
     psf2_field.grid(row=7, column=1, sticky="EW")
-    s5.configure(command=psf2_field.xview)
+    s5.configure(command=psf2_field.xview, bg='white')
 
-    tk.Button(self.dcd_compare_tab, text='DCDs 2', command=lambda:self._select_dcd2_compare(dcd2_field)).grid(row=9, column=0, sticky="EW")
+    tk.Button(self.dcd_compare_tab, text='DCDs 2', command=lambda:self._select_dcd2_compare(dcd2_field), bg='white', fg='black', highlightbackground='white').grid(row=9, column=0, sticky="EW")
     s6 = self._add_horisontal_scroll(self.dcd_compare_tab, row=10, column=1)
-    dcd2_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s6.set)
+    dcd2_field = tk.Entry(self.dcd_compare_tab, state='disabled', xscrollcommand=s6.set, bg='white', fg='black', highlightbackground='white', disabledbackground=self.gray, disabledforeground='black')
     dcd2_field.grid(row=9, column=1, sticky="EW")
-    s6.configure(command=dcd2_field.xview)
+    s6.configure(command=dcd2_field.xview, bg='white')
 
-    tk.Label(self.dcd_compare_tab, text='Name 2: ', anchor='w').grid(row=11, column=0, sticky='W')
+    tk.Label(self.dcd_compare_tab, text='Name 2: ', anchor='w', bg='white', fg='black').grid(row=11, column=0, sticky='W')
     # self.sim_name1 = tk.StringVar(value='sim1')
-    field2 = tk.Frame(self.dcd_compare_tab)
+    field2 = tk.Frame(self.dcd_compare_tab, bg='white')
     field2.grid(row=11, column=1, sticky="EW")
     field2.columnconfigure(0, weight=1)
-    self.compare_dcd2_name = tk.Entry(field2)
+    self.compare_dcd2_name = tk.Entry(field2, bg='white', fg='black', highlightbackground='white')
     self.compare_dcd2_name.insert(0, 'sim2') # remove when test resolved
     self.compare_dcd2_name.grid(row=11, column=0, sticky="EW")
-    l1 = tk.Label(self.dcd_compare_tab, text='Each simulation must be given a unique name, else the results will be overwritten.', anchor='w', justify='left')
+    l1 = tk.Label(self.dcd_compare_tab, text='Each simulation must be given a unique name, else the results will be overwritten.', anchor='w', justify='left', bg='white', fg='black')
     l1.grid(row=12, column=1, sticky='EW', pady=(0,24))
     l1.config(font=("Helvetica", 11))
     self.color_dcd2 = '#21c25f'
@@ -147,9 +147,9 @@ def compare_view(self):
     color_dcd_field2.bind("<Button-1>", lambda x=self.color_dcd2, y=color_dcd_field2:self._choose_dcd_color2(x, y))
 
     self.max_water_comp_dcd = tk.IntVar(value=3)
-    tk.Label(self.dcd_compare_tab, text='Maximum number of waters in the bridge', anchor="w").grid(row=13, column=0, sticky='W')
+    tk.Label(self.dcd_compare_tab, text='Maximum number of waters in the bridge', anchor="w", bg='white', fg='black').grid(row=13, column=0, sticky='W')
     ttk.Combobox(self.dcd_compare_tab, textvariable=self.max_water_comp_dcd, values=[1,2,3,4,5], state='readonly').grid(row=13, column=1, sticky="EW")
 
-    tk.Button(self.dcd_compare_tab, text='Construct graph', command=lambda:self._construct_compare_graphs(psf1=self.psf_1, psf2=self.psf_2, dcd1=self.dcd_1, dcd2=self.dcd_2)).grid(self._create_big_button_grid(14), columnspan=2)
+    tk.Button(self.dcd_compare_tab, text='Construct graph', command=lambda:self._construct_compare_graphs(psf1=self.psf_1, psf2=self.psf_2, dcd1=self.dcd_1, dcd2=self.dcd_2), bg='white', fg='black', highlightbackground='white').grid(self._create_big_button_grid(14), columnspan=2)
 
     self.compare_row = 15
--- cgraph/view/crystal_strucutre_analyser_view.py
+++ cgraph/view/crystal_strucutre_analyser_view.py
@@ -11,20 +11,20 @@ def csa_view(self):
     self.inputFrame.columnconfigure(1, weight=1)
 
     s1 = self._add_horisontal_scroll(self.inputFrame, row=2, column=1)
-    self._input_folder = tk.Entry(self.inputFrame, state='disabled', xscrollcommand=s1.set)
-    tk.Button(self.inputFrame, text='Select PDB folder', command=lambda:self._select_pdb_root_folder(self._input_folder), takefocus=False).grid(row=1, column=0, sticky="EW")
+    self._input_folder = tk.Entry(self.inputFrame, state='disabled', xscrollcommand=s1.set, bg='white', fg='black',  highlightbackground='white', disabledbackground=self.gray, disabledforeground='black')
+    tk.Button(self.inputFrame, text='Select PDB folder', command=lambda:self._select_pdb_root_folder(self._input_folder), takefocus=False, bg='white', fg='black', highlightbackground='white').grid(row=1, column=0, sticky="EW")
     self._input_folder.grid(row=1, column=1, sticky="EW", columnspan=2)
-    s1.configure(command=self._input_folder.xview)
+    s1.configure(command=self._input_folder.xview, bg='white')
 
-    tk.Button(self.inputFrame, text='Select reference file', command=lambda:self._select_reference_pdb(self._input_pdb)).grid(row=4, column=0, sticky="EW")
+    tk.Button(self.inputFrame, text='Select reference file', command=lambda:self._select_reference_pdb(self._input_pdb),  highlightbackground='white', bg='white', fg='black').grid(row=4, column=0, sticky="EW")
     s2 = self._add_horisontal_scroll(self.inputFrame, row=5, column=1)
-    self._input_pdb = tk.Entry(self.inputFrame, state='disabled', xscrollcommand=s2.set)
+    self._input_pdb = tk.Entry(self.inputFrame, state='disabled', xscrollcommand=s2.set, bg='white', fg='black',  highlightbackground='white',disabledbackground=self.gray, disabledforeground='black')
     self._input_pdb.grid(row=4, column=1, sticky="EW")
-    s2.configure(command=self._input_pdb.xview)
+    s2.configure(command=self._input_pdb.xview, bg='white')
 
 
     self.sequance_identity_threshold = tk.DoubleVar(value=75)
-    tk.Label(self.inputFrame, text='Minimum sequence identity (%)').grid(row=6, column=0, sticky='W')
+    tk.Label(self.inputFrame, text='Minimum sequence identity (%)', bg='white', fg='black').grid(row=6, column=0, sticky='W')
     ttk.Spinbox(self.inputFrame, textvariable=self.sequance_identity_threshold, from_=1, to=100, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 100)).grid(row=6, column=1, sticky="EW")
 
     # ----------------------- waterClusterFrame -----------------------
@@ -34,21 +34,21 @@ def csa_view(self):
     self.waterClusterFrame.columnconfigure(0, weight=1)
 
     # tk.Button(self.waterClusterFrame, text='Parameter analysis', command=self._perform_parameter_analysis, width=self.button_width).grid(row=0, column=0, padx=(self.padx,self.padx), pady=(self.pady,self.pady), sticky="EW")
-    waterClusterParameterFrame = tk.Frame(self.waterClusterFrame)
+    waterClusterParameterFrame = tk.Frame(self.waterClusterFrame, bg='white')
     waterClusterParameterFrame.grid(row=1, column=0, columnspan=4, sticky="EW")
-    lwcp = tk.Label(waterClusterParameterFrame, text='For the DBSCAN analysis the default parameters are recommended to use.', anchor='w')
+    lwcp = tk.Label(waterClusterParameterFrame, text='For the DBSCAN analysis the default parameters are recommended to use.', anchor='w', bg='white', fg='black')
     lwcp.grid(row=0, columnspan=4, sticky='EW')
-    lwcp.config(font=("Helvetica", 11))
+    lwcp.config(font=("Helvetica", 11), bg='white', fg='black')
 
-    tk.Label(waterClusterParameterFrame, text='DBSCAN eps:').grid(row=1, column=0, sticky="W")
+    tk.Label(waterClusterParameterFrame, text='DBSCAN eps:', bg='white', fg='black').grid(row=1, column=0, sticky="W")
     self.eps = tk.DoubleVar(value=1.4)
     ttk.Spinbox(waterClusterParameterFrame, textvariable=self.eps, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 100)).grid(row=1, column=1, sticky="W")
 
-    tk.Label(waterClusterParameterFrame, text='Superimposition RMS threshold:').grid(row=1, column=2, sticky="E")
+    tk.Label(waterClusterParameterFrame, text='Superimposition RMS threshold:', bg='white', fg='black').grid(row=1, column=2, sticky="E")
     self.superimposition_threshold = tk.DoubleVar(value=5)
     ttk.Spinbox(waterClusterParameterFrame, textvariable=self.superimposition_threshold, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 100)).grid(row=1, column=3, sticky="E")
 
-    tk.Button(self.waterClusterFrame, text='Calculate water clusters', command=self._init_water_clusters, width=self.button_width).grid(self._create_big_button_grid(2))
+    tk.Button(self.waterClusterFrame, text='Calculate water clusters', command=self._init_water_clusters, width=self.button_width, bg='white', fg='black',  highlightbackground='white').grid(self._create_big_button_grid(2))
 
 
     # ----------------------- conservedNetworkFrame -----------------------
@@ -59,41 +59,41 @@ def csa_view(self):
     self.conservedNetworkFrame.columnconfigure(1, weight=1)
 
 
-    selsting_frame = tk.Frame(self.conservedNetworkFrame)
+    selsting_frame = tk.Frame(self.conservedNetworkFrame, bg='white')
     selsting_frame.grid(row=8, column=0, columnspan=2, sticky="EW")
     selsting_frame.columnconfigure(1, weight=1)
     self.selection_string = tk.StringVar(value='protein')
     # tk.Label(selsting_frame, text='  Selection string', anchor="w").grid(row=8, column=0, sticky='W')
     # tk.Entry(selsting_frame, textvariable=self.selection_string).grid(row=8, column=1, sticky="EW")
 
-    tk.Label(self.conservedNetworkFrame, text='H-bond criteria ', anchor="w").grid(row=9, column=0, sticky='W')
-    hcritera_frame = tk.Frame(self.conservedNetworkFrame)
+    tk.Label(self.conservedNetworkFrame, text='H-bond criteria ', anchor="w", bg='white', fg='black').grid(row=9, column=0, sticky='W')
+    hcritera_frame = tk.Frame(self.conservedNetworkFrame, bg='white')
     hcritera_frame.grid(row=9, column=1, columnspan=5, sticky="EW")
     self.c_distance = tk.DoubleVar(value=3.5)
     self.c_cut_angle = tk.DoubleVar(value=60)
     ttk.Spinbox(hcritera_frame, textvariable=self.c_distance, from_=0, to=5, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 5)).grid(row=9, column=1, sticky='W')
-    tk.Label(hcritera_frame, text='Å distance     ', anchor="w").grid(row=9, column=2, sticky='W')
+    tk.Label(hcritera_frame, text='Å distance     ', anchor="w", bg='white', fg='black').grid(row=9, column=2, sticky='W')
     self.c_use_angle = tk.BooleanVar()
     self.c_use_angle.set(False)
-    tk.Checkbutton(hcritera_frame, variable=self.c_use_angle, anchor="w").grid(row=9, column=3, sticky='E')
+    tk.Checkbutton(hcritera_frame, variable=self.c_use_angle, anchor="w", bg='white', fg='black').grid(row=9, column=3, sticky='E')
     ttk.Spinbox(hcritera_frame, textvariable=self.c_cut_angle, from_=0, to=180, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 180)).grid(row=9, column=4, sticky='W')
-    tk.Label(hcritera_frame, text='degrees angle', anchor="w").grid(row=9, column=5, sticky='W')
+    tk.Label(hcritera_frame, text='degrees angle', anchor="w", bg='white', fg='black').grid(row=9, column=5, sticky='W')
 
     self.conservation_threshold = tk.DoubleVar(value=90)
-    tk.Label(self.conservedNetworkFrame, text='Conservation of H-bonding groups across structures', anchor="w").grid(row=10, column=0, sticky='W')
+    tk.Label(self.conservedNetworkFrame, text='Conservation of H-bonding groups across structures', anchor="w", bg='white', fg='black').grid(row=10, column=0, sticky='W')
     ttk.Spinbox(self.conservedNetworkFrame, textvariable=self.conservation_threshold, from_=1, to=100, width=5, validate="key", validatecommand=(self.ifnum_cmd, '%S', '%P', 0, 100)).grid(row=10, column=1, sticky="W")
 
-    tk.Label(self.conservedNetworkFrame, text='Plot for each structure:', anchor="w").grid(row=11, column=0, sticky='W')
+    tk.Label(self.conservedNetworkFrame, text='Plot for each structure:', anchor="w", bg='white', fg='black').grid(row=11, column=0, sticky='W')
     each_plots_crystal = tk.Frame(self.conservedNetworkFrame)
     each_plots_crystal.grid(row=11, column=1, columnspan=3, sticky="EW", pady=(0,10))
     self.is_induvidual_graph = tk.BooleanVar()
-    tk.Checkbutton(each_plots_crystal, text='Individual network    ', variable=self.is_induvidual_graph, anchor="w").grid(row=11, column=1, sticky='E')
+    tk.Checkbutton(each_plots_crystal, text='Individual network    ', variable=self.is_induvidual_graph, anchor="w", bg='white', fg='black').grid(row=11, column=1, sticky='E')
 
     self.is_difference_graph = tk.BooleanVar()
-    tk.Checkbutton(each_plots_crystal, text='Difference graph    ', variable=self.is_difference_graph, anchor="w").grid(row=11, column=2, sticky='E')
+    tk.Checkbutton(each_plots_crystal, text='Difference graph    ', variable=self.is_difference_graph, anchor="w", bg='white', fg='black').grid(row=11, column=2, sticky='E')
 
     self.is_linear_lenght_plot = tk.BooleanVar()
-    tk.Checkbutton(each_plots_crystal, text='Linear lengths', variable=self.is_linear_lenght_plot, anchor="w").grid(row=11, column=3, sticky='E')
+    tk.Checkbutton(each_plots_crystal, text='Linear lengths', variable=self.is_linear_lenght_plot, anchor="w", bg='white', fg='black').grid(row=11, column=3, sticky='E')
 
 
     # ----------------------- HbondNetworkFrame -----------------------
@@ -105,15 +105,15 @@ def csa_view(self):
 
     self.include_waters_hbond = tk.BooleanVar()
     self.include_waters_hbond.set(True)
-    tk.Checkbutton(self.HbondNetworkFrame, text='Include crystallographic waters', variable=self.include_waters_hbond, anchor="w").grid(self._create_big_button_grid(14))
+    tk.Checkbutton(self.HbondNetworkFrame, text='Include crystallographic waters', variable=self.include_waters_hbond, anchor="w", bg='white', fg='black').grid(self._create_big_button_grid(14))
 
     self.useWaterCoords = tk.BooleanVar()
-    tk.Checkbutton(self.HbondNetworkFrame, text='Use water cluster coordinates', variable=self.useWaterCoords, anchor="w").grid(self._create_big_button_grid(15))
+    tk.Checkbutton(self.HbondNetworkFrame, text='Use water cluster coordinates', variable=self.useWaterCoords, anchor="w", bg='white', fg='black').grid(self._create_big_button_grid(15))
 
     self.include_backbone_sidechain = tk.BooleanVar()
-    tk.Checkbutton(self.HbondNetworkFrame, text='Include sidechain-backbone interactions', variable=self.include_backbone_sidechain, anchor="w").grid(self._create_big_button_grid(16))
+    tk.Checkbutton(self.HbondNetworkFrame, text='Include sidechain-backbone interactions', variable=self.include_backbone_sidechain, anchor="w", bg='white', fg='black').grid(self._create_big_button_grid(16))
 
-    tk.Button(self.HbondNetworkFrame, text='Calculate conserved H-bond network', command=lambda:self._init_pdb_conserved_graph_analysis('hbond'), width=self.button_width).grid(self._create_big_button_grid(17))
+    tk.Button(self.HbondNetworkFrame, text='Calculate conserved H-bond network', command=lambda:self._init_pdb_conserved_graph_analysis('hbond'), width=self.button_width, bg='white', fg='black', highlightbackground='white').grid(self._create_big_button_grid(17))
 
 
     # ----------------------- WaterWireFrame -----------------------
@@ -124,9 +124,9 @@ def csa_view(self):
     self.WaterWireFrame.columnconfigure(1, weight=1)
 
     self.max_water = tk.IntVar(value=3)
-    tk.Label(self.WaterWireFrame, text=' Maximum number of waters in the bridge', anchor="w").grid(row=17, column=0, sticky='W')
+    tk.Label(self.WaterWireFrame, text=' Maximum number of waters in the bridge', anchor="w", bg='white', fg='black').grid(row=17, column=0, sticky='W')
     ttk.Combobox(self.WaterWireFrame, textvariable=self.max_water, values=[1,2,3,4,5], state='readonly').grid(row=17, column=1, sticky="EW")
-    tk.Button(self.WaterWireFrame, text='Calculate conserved water wire network', command=lambda:self._init_pdb_conserved_graph_analysis('water_wire'), width=self.button_width).grid(self._create_big_button_grid(18), columnspan=2)
+    tk.Button(self.WaterWireFrame, text='Calculate conserved water wire network', command=lambda:self._init_pdb_conserved_graph_analysis('water_wire'), width=self.button_width, bg='white', fg='black',  highlightbackground='white').grid(self._create_big_button_grid(18), columnspan=2)
 
     # self.completedText = tk.StringVar(value='')
     # self.completed = tk.Label(self.conservedNetworkFrame, textvariable=self.completedText)
