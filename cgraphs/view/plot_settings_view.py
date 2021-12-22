67c957d6e886135f0c8de7f404f963c9a4b6df44
evabertalan <eva0bertalan@gmail.com>
2021-12-22 10:33:41 +0100

    arrage grid items

--- cgraphs/view/plot_settings_view.py
+++ cgraphs/view/plot_settings_view.py
@@ -21,15 +21,15 @@ def plot_settings(self):
     settings_options = {
                 'edge_width': {'label': 'Edge width',
                 'default': '2'},
-                'node_label_size': {'label': 'Node label size',
-                'default': '12'},
                 'edge_label_size': {'label': 'Edge label size',
                 'default': '10'},
                 'node_size': {'label': 'Node size',
                 'default': '150'},
+                'node_label_size': {'label': 'Node label size',
+                'default': '12'},
                 'plot_title_fontsize': {'label': 'Plot title font size',
                 'default': '20'},
-                'plot_label_fontsize': {'label': 'Plot label font size',
+                'plot_label_fontsize': {'label': 'Plot axis label font size',
                 'default': '36'},
                 'plot_tick_fontsize': {'label': 'Plot tick font size',
                 'default': '33'},
@@ -46,36 +46,36 @@ def plot_settings(self):
         _create_entries(self, main_frame, row, key, value)
 
 
-    tk.Label(main_frame, text='Graph color', anchor='w',  bg='white', fg='black').grid(row=i+1, column=0, sticky='W')
+    tk.Label(main_frame, text='Graph color', anchor='w',  bg='white', fg='black').grid(row=i+1, column=0, sticky='W', pady=(2,2), padx=(2,2))
     self.graph_color = 'gray'
     graph_color_field = tk.Label(main_frame, width=2, bg=self.graph_color, anchor="w")
-    graph_color_field.grid(row=i+1, column=1, sticky='W')
+    graph_color_field.grid(row=i+1, column=1, sticky='W', pady=(2,2), padx=(2,2))
     graph_color_field.bind("<Button-1>", lambda x=self.graph_color, y=graph_color_field, var='graph_color':self._choose_color(x, y, var))
 
-    tk.Label(main_frame, text='Water node color', anchor='w',  bg='white', fg='black').grid(row=i+2, column=0, sticky='W')
+    tk.Label(main_frame, text='Water node color', anchor='w',  bg='white', fg='black').grid(row=i+2, column=0, sticky='W', pady=(2,2), padx=(2,2))
     self.water_node_color = '#db5c5c'
     water_node_color_field = tk.Label(main_frame, width=2, bg=self.water_node_color, anchor="w")
-    water_node_color_field.grid(row=i+2, column=1, sticky='W')
+    water_node_color_field.grid(row=i+2, column=1, sticky='W', pady=(2,2), padx=(2,2))
     water_node_color_field.bind("<Button-1>", lambda x=self.water_node_color, y=water_node_color_field, var='water_node_color':self._choose_color(x, y, var))
 
-    tk.Label(main_frame, text='Difference graph color', anchor='w',  bg='white', fg='black').grid(row=i+3, column=0, sticky='W')
+    tk.Label(main_frame, text='Difference graph color', anchor='w',  bg='white', fg='black').grid(row=i+3, column=0, sticky='W', pady=(2,2), padx=(2,2))
     self.difference_graph_color = '#129fe6'
     difference_graph_color_field = tk.Label(main_frame, width=2, bg=self.difference_graph_color, anchor="w")
-    difference_graph_color_field.grid(row=i+3, column=1, sticky='W')
+    difference_graph_color_field.grid(row=i+3, column=1, sticky='W', pady=(2,2), padx=(2,2))
     difference_graph_color_field.bind("<Button-1>", lambda x=self.difference_graph_color, y=difference_graph_color_field, var='difference_graph_color':self._choose_color(x, y, var))
 
-    tk.Label(main_frame, text='Save figures in formats:', anchor='w',  bg='white', fg='black').grid(row=i+4, column=0, sticky='W')
+    tk.Label(main_frame, text='Save figures in formats', anchor='w',  bg='white', fg='black').grid(row=i+4, column=0, sticky='W')
     self.png = tk.BooleanVar()
     self.png.set(True)
-    tk.Checkbutton(main_frame, text='png', state='disabled', variable=self.png, anchor="w", bg='white', fg='black').grid(row=i+4, column=1, sticky='E')
+    tk.Checkbutton(main_frame, text='png', state='disabled', variable=self.png, anchor="w", bg='white', fg='black').grid(row=i+4, column=1, sticky='W')
 
     self.eps = tk.BooleanVar()
     self.eps.set(False)
-    tk.Checkbutton(main_frame, text='eps', variable=self.eps, anchor="w", bg='white', fg='black').grid(row=i+5, column=1, sticky='E')
+    tk.Checkbutton(main_frame, text='eps', variable=self.eps, anchor="w", bg='white', fg='black').grid(row=i+5, column=1, sticky='W')
 
     self.svg = tk.BooleanVar()
     self.svg.set(False)
-    tk.Checkbutton(main_frame,text='svg', variable=self.svg, anchor="w", bg='white', fg='black').grid(row=i+6, column=1, sticky='E')
+    tk.Checkbutton(main_frame,text='svg', variable=self.svg, anchor="w", bg='white', fg='black').grid(row=i+6, column=1, sticky='W')
 
-    tk.Button(main_frame, text='Save', bg='white', fg='black', command=self._save_plot_settings, takefocus=False,  highlightbackground='white').grid(row=i+7, column=2, sticky="EW", columnspan=2)
+    tk.Button(main_frame, text='Save', bg='white', fg='black', command=self._save_plot_settings, takefocus=False,  highlightbackground='white').grid(row=i+7, column=0, sticky="EW", columnspan=2)
 
