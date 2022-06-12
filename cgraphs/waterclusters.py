som bug fixes: * move plot parameters to helperfile * only create node color gradient map when matching node list is not empty * water cluser plot using customizable plot parameters

cgraphs/waterclusters.py
@@ -143,7 +143,7 @@ class WaterClusters(ProteinGraphAnalyser):
 
         fig, ax = _hf.create_plot(title='Projection of the '+str(self.n_clusters_)+' water clusters and '+str(n_noise_)+' outlier points',
                                       xlabel='PCA projected xy plane',
-                                      ylabel='Z coordinates')
+                                      ylabel='Z coordinates', plot_parameters=self.plot_parameters)
 
         colormap = plt.cm.get_cmap('tab20', len(set(self.labels)))
         mycolors = colormap(np.linspace(0, 1, len(set(self.labels))))
@@ -221,7 +221,7 @@ class WaterClusters(ProteinGraphAnalyser):
 
         fig, ax = _hf.create_plot(title='Projection of all water molecules',
                                  xlabel='PCA projected xy plane',
-                                 ylabel='Z coordinates')
+                                 ylabel='Z coordinates', plot_parameters=self.plot_parameters)
         ax.scatter(xy, self.water_coordinates[:,2], s=18, c='darkblue')
         plt.savefig(self.water_cluster_folder+'all_water_projection.png')
         plt.close()