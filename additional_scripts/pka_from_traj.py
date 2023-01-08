68143e54094ea504e0b0e75a5b6bd5c8ad640d1c
evabertalan <eva0bertalan@gmail.com>
2023-01-08 08:02:06 +0100

    function to write avg trajecroty pKa as extrnal data file for PDB

--- additional_scripts/pka_from_traj.py
+++ additional_scripts/pka_from_traj.py
@@ -13,17 +13,20 @@ class pKa_from_traj:
         print('Number of residues in selection:', len(self.u.residues))
         print('Number of frames', len(self.u.trajectory))
 
+
     def compute_pka_for_traj(self, selection='protein'):
         self.selection = selection
         print('Selection', self.selection)
         self.pkatraj = PropkaTraj(self.u, select=selection)
         self.pkatraj.run()
 
+
     def get_pka_for_frame(self, write_to_file=None):
         pkas = self.pkatraj.pkas
         if write_to_file:
             pkas.to_csv(write_to_file)
         return pkas
+
 
     def get_pka_statistic(self, selection=None, write_to_file=None):
         resids = self._get_selected_res(selection)
@@ -32,9 +35,10 @@ class pKa_from_traj:
              stats.to_csv(write_to_file)
         return stats
 
-    def plot_pka_time_series_for_selection(self, selection=None, write_to_file=None):
+
+    def plot_pka_time_series_for_selection(self, selection=None, write_to_file=None, figsize=None):
         resids = self._get_selected_res(selection)
-        t = selection if selection else self.selection
+        t = selection if selection else pka_tr.selection
         fig, ax = self._create_plot(title=t, xlabel='Time (ns)', ylabel=r'p$K_a$')
         for col in resids:
             ax.plot(self.pkatraj.pkas[col], label=col)
@@ -43,17 +47,37 @@ class pKa_from_traj:
         if write_to_file:
             fig.savefig(write_to_file)
 
-    def plot_pka_statistic_for_selection(self, selection=None, write_to_file=None):
+
+    def plot_pka_statistic_for_selection(self, selection=None, write_to_file=None, figsize=None):
         resids = self._get_selected_res(selection)
-        t = selection if selection else self.selection
-        fig, ax = self._create_plot(title=t, xlabel='Res ID', ylabel=r'p$K_a$')
+        t = selection if selection else pka_tr.selection
+        fig, ax = self._create_plot(title=t, xlabel='Res ID', ylabel=r'p$K_a$', figsize=figsize)
         sns.boxplot(data=self.pkatraj.pkas[resids], ax=ax)
 
         if write_to_file:
             fig.savefig(write_to_file)
 
+
     def write_pka_to_external_data_file(self, write_to_file, selection=None):
-        pass
+        try:
+            write_to_file.endswith('_data.txt')
+            stats = self.get_pka_statistic(selection)
+
+            res_name_map = {}
+            prot = self.u.select_atoms('protein')
+            for res in prot.residues:
+                res_id = res.resid
+                res_name = res.resname
+                seg_id = res.segid
+                res_name_map.update({f'{res_id}': {'res_name': res_name, 'seg_id': seg_id} })
+
+            with open(write_to_file, 'w') as f:
+                for res_id, avg_pka in zip(stats.columns, stats.iloc[1]):
+                    line = ' '.join([res_name_map[str(res_id)]['res_name'], str(res_id), res_name_map[str(res_id)]['seg_id'], str(avg_pka)])
+                    f.write(line + '\n')
+        except:
+            print('Name of external data file should end with _data.txt/n The first part of the name should match the PDB file analized e.g: 6b73_data.txt')
+
 
     def _get_selected_res(self, selection):
         if selection:
@@ -62,7 +86,9 @@ class pKa_from_traj:
             resids = self.pkatraj.pkas.columns
         return resids
 
-    def _create_plot(self, title='', xlabel='', ylabel='',figsize=(12, 8)):
+
+    def _create_plot(self, title='', xlabel='', ylabel='', figsize=None):
+        figsize = figsize if figsize is not None else (12, 8)
         fig, ax = plt.subplots(figsize=figsize)
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
