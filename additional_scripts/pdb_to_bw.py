9771131408332517b5bfc6967270985febcb6ff8
evabertalan <eva0bertalan@gmail.com>
2023-07-30 11:07:17 +0200

    add BW number assignment from genreic numbering GPCRdb endboint in some cases the generic numbering is not available to download, in theses cases we can upload the PDB file to the server which sends back the PDB with assigned generic numbers * in the PDB file the genric numbers are written in the location of the B-factor column of the first atom of the given resiude

--- additional_scripts/pdb_to_bw.py
+++ additional_scripts/pdb_to_bw.py
@@ -52,3 +52,25 @@ def renumber_pdb_to_bw(pdb_id, pdb_file, target_folder):
     uniprot_id = get_uniport_from_pdb(pdb_id)
     seq_details = get_detailed_sequence_numbering(uniprot_id, save_to=target_folder, pdb_id=pdb_id)
     write_renumbered_pdb_file(pdb_file, pdb_id, seq_details, target_folder)
+
+
+def assign_generic_numbers(pdb_code, pdb_file):
+    post_url = 'https://services/structure/assign_generic_numbers'
+    response = requests.post(post_url, data=pdb_file)
+    jsonRes = response.json()
+    print(jsonRes)
+
+def write_renumbered_pdb_file_from_generic_PDB(pdb_id, generic_pdb, target_folder):
+    protein_struct = _mda.Universe(generic_pdb)
+    og_struct = _mda.Universe(generic_pdb)
+    bw_numbers = {}
+    for i, res  in enumerate(og_struct.residues):
+        bw_numbers.update({f'{res.resname}-{res.resid}': round(res.atoms[0].tempfactor*100)})
+
+    for i, atom  in enumerate(og_struct.atoms):
+        bw_number = bw_numbers[f'{atom.resname}-{atom.resid}']
+        if atom.resname in amino_dict.keys() and bw_number < 900:
+            protein_struct.atoms[i].residue.resid = bw_number
+            protein_struct.atoms[i].residue.resname = 'BWX'
+    protein_struct.atoms.write(f'{target_folder}/{str(pdb_id)}_bw.pdb')
+
